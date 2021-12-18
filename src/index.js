d89f8b12953de039065236761d31919df52e925d
AranBeitia <arantza.beitia@gmail.com>
2021-12-18 20:54:51 +0100

    cors

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ const clientRouter = require('./routes/client.routes')
 const productRouter = require('./routes/product.routes')
 const orderRouter = require('./routes/order.routes')
 
+const cors = require('cors')
+
 // connect mongo
 mongoose.Promise = global.Promise
 mongoose.connect('mongodb://127.0.0.1:27017/REST-APIS', {
@@ -20,6 +22,9 @@ const app = express()
 app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(fileUpload({ createParentPath: true }))
+
+// cors
+app.use(cors())
 
 // app routes
 app.use(clientRouter)
