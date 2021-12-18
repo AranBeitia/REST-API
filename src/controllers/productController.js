e659479c07cf07a99b6512915081ab468da83620
AranBeitia <arantza.beitia@gmail.com>
2021-12-18 17:47:13 +0100

    product api

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -2,15 +2,13 @@ const Products = require('../models/Products')
 
 async function newProduct(req, res, next) {
 	const product = new Products(req.body)
+
 	try {
 		const image = req.files.image
 		if (image) {
-			const path = __dirname + '/uploads/' + image.name
-
+			const path = process.cwd() + '/src/uploads/' + image.name
 			image.mv(path, function (err) {
-				if (err) {
-					return res.status(500).send(err)
-				}
+				if (err) res.status(500).send(err)
 			})
 
 			product.image = image.name
@@ -22,6 +20,48 @@ async function newProduct(req, res, next) {
 	}
 }
 
+async function showAllProducts(req, res, next) {
+	try {
+		const data = await Products.find({})
+		res.status(200).send({ success: true, data })
+	} catch (error) {
+		next(error)
+	}
+}
+
+async function showOneProduct(req, res, next) {
+	try {
+		const data = await Products.findById(req.params.productId)
+		res.status(200).send({ success: true, data })
+	} catch (error) {
+		next(error)
+	}
+}
+
+async function updateProduct(req, res, next) {
+	try {
+		data = await Products.findOneAndUpdate(req.params.productId)
+		res.status(200).send({ success: true, data })
+	} catch (error) {
+		next(error)
+	}
+}
+
+async function deleteProduct(req, res, next) {
+	try {
+		await Products.findByIdAndDelete(req.params.productId)
+		res
+			.status(200)
+			.send({ success: true, message: 'The product has been deleted' })
+	} catch (error) {
+		next(error)
+	}
+}
+
 module.exports = {
 	newProduct,
+	showAllProducts,
+	showOneProduct,
+	updateProduct,
+	deleteProduct,
 }
--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -3,5 +3,9 @@ const productRouter = express.Router()
 const productController = require('../controllers/productController')
 
 productRouter.post('/products', productController.newProduct)
+productRouter.get('/products', productController.showAllProducts)
+productRouter.get('/products/:productId', productController.showOneProduct)
+productRouter.patch('/products/:productId', productController.updateProduct)
+productRouter.delete('/products/:productId', productController.deleteProduct)
 
 module.exports = productRouter
